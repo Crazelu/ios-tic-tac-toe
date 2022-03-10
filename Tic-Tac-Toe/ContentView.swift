df950eac994993b9431b6a66de0913dddff77571
Crazelu <eberelucky64@gmail.com>
2022-03-10 22:46:03 +0100

    Initial Commit

---
+++ Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Crazelu on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Crazelu on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
